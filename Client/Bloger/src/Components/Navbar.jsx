ea78ac6ef898ca2a0f86126c0940cc3e331363e7
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-10-08 23:46:12 +0530

    client side complete

--- Client/Bloger/src/Components/Navbar.jsx
+++ Client/Bloger/src/Components/Navbar.jsx
@@ -8,16 +8,16 @@ function Navbar() {
        <img src="https://www.logoai.com/uploads/output/2021/12/21/a40b79883497c9cf19e38b2bea41a4a7.jpg " alt="" className='w-[120px] h-[120px]' />
       </div>
       <div className='flex gap-3 uppercase '>
-        <Link className='hover:underline' to='/'>Art</Link>
-        <Link className='hover:underline' to='/'>Science</Link>
-        <Link className='hover:underline' to='/'>Technology</Link>
-        <Link className='hover:underline' to='/'>Cinema</Link>
-        <Link className='hover:underline' to='/'>Design</Link>
-        <Link className='hover:underline' to='/'>Food</Link>
-        <Link className='hover:underline' to='/'>Join</Link>
-        <Link className='hover:underline' to='/'>Logout</Link>
+        <Link className='hover:underline' to='/single'>Art</Link>
+        <Link className='hover:underline' to='/single'>Science</Link>
+        <Link className='hover:underline' to='/single'>Technology</Link>
+        <Link className='hover:underline' to='/single'>Cinema</Link>
+        <Link className='hover:underline' to='/single'>Design</Link>
+        <Link className='hover:underline' to='/single'>Food</Link>
+        <Link className='hover:underline' to='/single'>Join</Link>
+        <Link className='hover:underline' to='/single'>Logout</Link>
         <div className='hover:bg-slate-600 px-8 py-2 hover:text-white border rounded-full'>
-        <Link to='/'>Write</Link>
+        <Link to='/write'>Write</Link>
         </div>
       
     
--- Client/Bloger/src/Pages/Write.jsx
+++ Client/Bloger/src/Pages/Write.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function Write() {
+  const [value, setValue] = useState('');
   return (
-    <div>
+    <div className='grid grid-cols-4 gap-3 mx-10 mb-8'>
+      <div className='col-span-3'>
+        <input type="text" placeholder='Type Title Here' className='border w-full my-2 p-3 outline-none' />
+        <ReactQuill theme="snow" value={value} onChange={setValue} className='w-full h-[50%]'/>;
+      </div>
+      <div className='col-span-1'>
+        <div className='border p-3 rounded-lg mb-3'>
+          <h1 className='text-center font-bold text-gray-700 text-[20px]'>Publish</h1>
+          <ul>
+            <li>Status : Draft</li>
+            <li>Visibility : Public</li>
+            <label htmlFor="">Upload Image</label>
+             <input type="file" placeholder='Upload Images' />
+          </ul>
+          <button>Save as draft</button>
+          <button>Update</button>
+
+        </div>
+        <div className='border p-3 rounded-lg'>
+          <h1 className='text-center font-bold text-gray-700 text-[20px]'>Category</h1>
+          <ul>
+            <li><input type="checkbox" className='border'/>Science</li>
+            <li><input type="checkbox" />Technology</li>
+            <li><input type="checkbox" />Cinema</li>
+            <li><input type="checkbox" />Design</li>
+            <li><input type="checkbox" />Food</li>
+         
+          </ul>
+         
+
+        </div>
+
+      </div>
+      
       
     </div>
   )
